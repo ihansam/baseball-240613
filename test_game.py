5056a4c97f9b68349e3a7619cef52c1cf2611f8e
kidan.jin <ihansam0322@gmail.com>
2024-06-13 13:51:26 +0900

    [feature] 1. None input

--- test_game.py
+++ test_game.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
 
+from game import Game
+
 
 class TestGame(TestCase):
-    pass
+    def test_exception_when_input_is_none(self):
+        self.game = Game()
+        with self.assertRaises(TypeError):
+            self.game.guess(None)
