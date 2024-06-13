2198ef9e2586d749dacac979c838abc027e1c4d8
kidan.jin <ihansam0322@gmail.com>
2024-06-13 14:36:20 +0900

    [refactoring] 5. guess correct

--- game_result.py
+++ game_result.py
@@ -1,5 +1,17 @@
 class GameResult:
     def __init__(self, solved, strikes, balls):
-        self.balls = balls
-        self.strikes = strikes
-        self.solved = solved
+        self.__balls = balls
+        self.__strikes = strikes
+        self.__solved = solved
+
+    @property
+    def solved(self):
+        return self.__solved
+
+    @property
+    def strikes(self):
+        return self.__strikes
+
+    @property
+    def balls(self):
+        return self.__balls
