59f7f4e09e3c219eda0276f203bd9c33ea5950f5
kidan.jin <ihansam0322@gmail.com>
2024-06-13 14:11:51 +0900

    [refactoring] 4. duplicate number input

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 class Game:
     def guess(self, guess_number):
+        self.assert_illegal_value(guess_number)
+
+    def assert_illegal_value(self, guess_number):
         if guess_number is None:
             raise ValueError
-
         if len(guess_number) != 3:
             raise ValueError
-
         for number in guess_number:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise ValueError
-
-        if len(set(guess_number)) != 3:
+        if self.has_duplicate_number(guess_number):
             raise ValueError
+
+    @staticmethod
+    def has_duplicate_number(guess_number):
+        return len(set(guess_number)) != 3
